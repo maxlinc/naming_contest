fa715c2b76636124d4fe5040941f61280bbc22c2
mlincoln <mlincoln@thoughtworks.com>
2013-03-06 15:39:50 -0300

    Only allow Google auth, add logout link

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -211,4 +211,5 @@ Devise.setup do |config|
   OmniAuth.config.logger = Logger.new(STDOUT)
   OmniAuth.logger.progname = "omniauth"
   config.omniauth :open_id, :store => OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id', :require => 'omniauth-openid'
+  config.scoped_views = true
 end
